6a60dee753e205ef17ba9b5dbad4271d3b233501
teddymcw <teddymcw@gmail.com>
2014-04-16 14:13:00 -0400

    finished setting up test directory

---
+++ tests/test1.py
@@ -0,0 +1,16 @@
+import unittest
+from google_translate_py2_funcs import get_trans_, get_lang_list, get_possible_lang
+
+class TestTranslateFuncs(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+	def test_get_trans_():
+		pass
+
+	def test_lang_list():
+		pass
+
+	def test_get_possible_lang():
+		pass
