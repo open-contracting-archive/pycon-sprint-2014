fd00b94437ba9ed1ffbb3f9ea9602211c2847ce3
Sarah Bird <sarah@bonvaya.com>
2014-04-27 19:15:40 -0700

    Get test1.py running

--- tests/test1.py
+++ tests/test1.py
@@ -1,27 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import unittest
-import codecs 
-from google_translate_py2_funcs import get_trans_, get_lang_list, get_possible_lang
+import codecs
+from translation.google_tranlate_py2_funcs import (
+    get_trans_,
+    get_lang_list,
+    get_possible_langs
+)
+from .api_key import API_KEY
+
 
 class TestTranslateFuncs(unittest.TestCase):
-	def setUp(self):
-		self.api_key = "AIzaSyCZBVx2a3tzKTW9TmTXoB3KJ_Z7T9PEspE"
-		self.api_key_unic = "AIzaSyCZBVx2a3tzKTW9TmTXoB3KJ_Z7T9PEspE"
-		self.text_French = "La litterature francaise comprend l"
-		self.text_German = "Contracting (englisch die Kontrahierung bzw. adjektivisch vertragschlieend)"
-		self.text_Georgian = "გამარტივებული ელექტრონული ტენდერი; კონსოლიდირებული ტენდერი; ელექტრონული ტენდერი; შესყიდვის ელექტრონული პროცედურა".encode('utf-8')
-		pass
+    def setUp(self):
+        self.api_key = API_KEY
+        self.text_French = "La litterature francaise comprend l"
+        self.text_German = "Contracting (englisch die Kontrahierung bzw. adjektivisch vertragschlieend)"  # nopep8
+        self.text_Georgian = "გამარტივებული ელექტრონული ტენდერი; კონსოლიდირებული ტენდერი; ელექტრონული ტენდერი; შესყიდვის ელექტრონული პროცედურა"  # nopep8
 
+    def test_get_trans_(self):
+        pass
 
-	def test_get_trans_(self):
-		pass
+    def test_get_lang_list(self):
+        self.assertEqual([u'fr', u'de'],
+                         get_lang_list([self.text_French, self.text_German],
+                                       self.api_key))
 
-	def test_get_lang_list(self):
-		self.assertEqual([u'fr', u'de'], get_lang_list([self.text_French, self.text_German]))
-		pass
-
-	def test_get_possible_lang():
-		pass
+    def test_get_possible_lang(self):
+        pass
 
 if __name__ == '__main__':
-	unittest.main()
-
+    """
+    To run these tests:
+        - in the tests directory, create a file called api_key.py and enter
+        your google translate api_key on a line `API_KEY = "myapikey"`
+        - then from the parent directory (root of pycon-sprint-2014)
+        use `python -m tests.test1`
+    """
+    unittest.main()
